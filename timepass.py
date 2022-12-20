7a8cdc76dc0fb0a279982cbc22006e23917a6324
SyedMohammed <syedmohdah@gmail.com>
2022-12-20 19:18:33 +0530

    First Commit

--- timepass.py
+++ timepass.py
@@ -1,13 +1,26 @@
 import random
+plater=0
+bot=0
+print("This is a Game of Rock,Paper,Scissor")
+number=int(input("How times you Want to Play:\n"))
 option=("Rock","Paper","Scissor")
 print(option)
-computer=random.choice(option)
-user=input("Enter any one of the option:\n")
-print(computer)
-if computer==user:
-    print(f"its is a tie {user} & {computer}")
-elif(user=="Rock" and computer=="Paper")or(user=="Paper" and computer=="Rock") or (user=="Scissor" and computer=="Paper"):
-   print(f"You choose {user} & computer choose {computer}\n So You Won:)")
-elif(user=="Scissor" and computer=="Rock") or (user=="Paper" and computer=="Scissor") or (user=="Rock" and computer=="Scissor"):
- print(f"You have lost the game you choose {user} computer choose {computer} :(")
- 
+for i in range(number):
+    computer=random.choice(option)
+    user=input("Enter any one of the option:\n")
+    print(computer)
+    if computer==user:
+        print(f"its is a tie {user} & {computer}")
+    elif(user=="Rock" and computer=="Paper")or(user=="Paper" and computer=="Rock") or (user=="Scissor" and computer=="Paper"):
+        plater+=1
+        print(f"You choose {user} & computer choose {computer}\n So You Won:)")
+    elif(user=="Scissor" and computer=="Rock") or (user=="Paper" and computer=="Scissor") or (user=="Rock" and computer=="Scissor"):
+        bot+=1
+        print(f"You have lost the game you choose {user} computer choose {computer} :(")
+if plater>bot:
+    print("User Have Won")
+elif plater==bot:
+    print("It is Tie Between You & Bot")
+else:
+    print("Bot Have Won")
+
