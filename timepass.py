c3838eb576f3dd16a0fa1df540f370ae6de1317f
SyedMohammed <syedmohdah@gmail.com>
2022-12-22 20:19:42 +0530

    Second Commit

--- timepass.py
+++ timepass.py
@@ -1,26 +1,34 @@
 import random
-plater=0
+import time
+player=0
 bot=0
+player_score=0
+bot_score=0
 print("This is a Game of Rock,Paper,Scissor")
 number=int(input("How times you Want to Play:\n"))
-option=("Rock","Paper","Scissor")
-print(option)
+option=("r","p","s")
 for i in range(number):
     computer=random.choice(option)
-    user=input("Enter any one of the option:\n")
+    time.sleep(1)
+    user=input("Enter any one of the option \nr for Rock \np for Paper \ns for Scissor:\n")
     print(computer)
     if computer==user:
+        time.sleep(1)
         print(f"its is a tie {user} & {computer}")
-    elif(user=="Rock" and computer=="Paper")or(user=="Paper" and computer=="Rock") or (user=="Scissor" and computer=="Paper"):
-        plater+=1
-        print(f"You choose {user} & computer choose {computer}\n So You Won:)")
-    elif(user=="Scissor" and computer=="Rock") or (user=="Paper" and computer=="Scissor") or (user=="Rock" and computer=="Scissor"):
+    elif(user=="r" and computer=="s")or(user=="p" and computer=="r") or (user=="s" and computer=="p"):
+        player+=1
+        player_score+=1
+        time.sleep(1)
+        print(f"You choose {user} & computer choose {computer}\nSo You Won 🙂")
+    elif(user=="s" and computer=="r") or (user=="p" and computer=="s") or (user=="r" and computer=="p"):
         bot+=1
-        print(f"You have lost the game you choose {user} computer choose {computer} :(")
-if plater>bot:
-    print("User Have Won")
-elif plater==bot:
-    print("It is Tie Between You & Bot")
+        bot_score+=1
+        time.sleep(1)
+        print(f"You  choose {user} & computer choose {computer}\nSo You lost☹")
+if player>bot:
+    print(f"User Have Won with {player_score} points")
+elif player==bot:
+    print(f"It is Tie Between You with {player_score} & Bot with {bot_score}")
 else:
-    print("Bot Have Won")
+    print(f"Bot Have Won with {bot_score} points")
 
